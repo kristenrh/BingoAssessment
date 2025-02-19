885d8ec8aa71b413ac4563bf62d820a68c96e8d3
Kristen Henningfeld <henningfeldk3407@my.uwstout.edu>
2025-02-18 21:01:49 -0600

    added random and manual games, and comments

--- src/App.java
+++ src/App.java
@@ -1,18 +1,139 @@
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.Random;
 
 public class App {
+    public static List<Integer> calls = new ArrayList<>(); // List that stores previous calls
+
+    // Main method to start the Bingo game
     public static void main(String[] args) {
+        // Retrieving cards from file
+        File file = new File("BingoCards.txt");
+        List<BingoCard> cards = new ArrayList<>();
         try {
-            File file = new File("BingoCards.txt");
-            List<BingoCard> cards = BingoCard.readFromFile(file);
-            
-            for (BingoCard card : cards) {
-                System.out.println("Created BingoCard with name: " + card.getName());
-                card.printCard();
-            }
+            cards = BingoCard.readFromFile(file);
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        for (BingoCard card : cards) {
+            card.printCard();
+            System.out.println();
+        }
+
+        // Game selection
+        Scanner userScanner = new Scanner(System.in);
+        System.out.println("Please select your game mode(manual/random): ");
+        String mode = userScanner.nextLine();
+        if (mode.equals("random")) {
+            randomGame();
+            calls.clear();
+        } else {
+            System.out.println("Select your card(1-9, excluding 6): ");
+            BingoCard manualCard = cards.get(userScanner.nextInt() - 1);
+            manualGame(manualCard);
+        }
+
+        userScanner.close();
+    }
+
+    // Method to play the game in random mode
+    public static void randomGame() {
+        List<BingoCard> randomCards = new ArrayList<>();
+        Scanner randomScanner = new Scanner(System.in);
+        int randomNumber;
+        Random random = new Random();
+
+        System.out.println("How many cards would you like to play with?(1-8): ");
+        int num = randomScanner.nextInt();
+        randomScanner.nextLine();
+        for (int i = 0; i < num; i++) {
+            randomNumber = random.nextInt(7) + 1; // generates a number between 1 and 8
+            BingoCard card = BingoCard.bingoCards.get(randomNumber);
+            randomCards.add(card);
+        }
+        System.out.println("Here are your cards for this game:");
+        for (BingoCard card : randomCards) {
+            card.printCard();
+        }
+        String win = "no";
+        while (win.equals("no")) {
+            int crossOffNum = call();
+            for (BingoCard card : randomCards) {
+                card.crossOff(crossOffNum);
+            }
+            for (BingoCard card : randomCards) {
+                card.printCard();
+            }
+            System.out.println("Do you have a BINGO?(yes/no)");
+            win = randomScanner.nextLine();
+            if (win.equals("yes")) {
+                for (BingoCard card : randomCards) {
+                    if (card.evaluate() == true) {
+                        System.out.println("Congratulations! You won BINGO!");
+                        System.exit(0);
+                    } else {
+                        System.out.println("Sorry, you do not have BINGO. You are losing a card from your pile.");
+                        randomCards.remove(0);
+                        win = "no";
+                        if (randomCards.isEmpty() == true) {
+                            System.out.println("Sorry, you're all out of cards. You lost BINGO.");
+                            System.exit(0);
+                        }
+                    }
+                }
+            }
+        }
+        randomScanner.close();
+    }
+
+    // Method to play the game in manual mode
+    public static void manualGame(BingoCard card) {
+        String win = "no";
+        Scanner manualScanner = new Scanner(System.in);
+
+        while (win.equals("no")) {
+            System.out.println("What is your call?(ex. B6): ");
+            String call = manualScanner.nextLine();
+            card.printCard();
+            card.crossOff(Integer.parseInt(call.substring(1)));
+            System.out.println("Do you have a BINGO?(yes/no)");
+            win = manualScanner.nextLine();
+            if (win.equals("yes")) {
+                if (card.evaluate() == true) {
+                    System.out.println("Congratulations! You won BINGO!");
+                    System.exit(0);
+                } else {
+                    System.out.println("Sorry, your card is not a winner. You lost BINGO.");
+                    System.exit(0);
+                }
+            }
+        }
+        manualScanner.close();
+    }
+
+    // Method to call a random number for the Bingo game
+    public static int call() {
+        Random num = new Random();
+        int randomNumber = num.nextInt(74) + 1; // generates a number between 1 and 75
+        while (calls.contains(randomNumber)) {
+            randomNumber = num.nextInt(74) + 1; // generates a number between 1 and 75
+        }
+        calls.add(randomNumber);
+
+        if (randomNumber < 16) {
+            System.out.println("You pulled B" + randomNumber);
+        } else if (randomNumber < 30) {
+            System.out.println("You pulled I" + randomNumber);
+        } else if (randomNumber < 46) {
+            System.out.println("You pulled N" + randomNumber);
+        } else if (randomNumber < 61) {
+            System.out.println("You pulled G" + randomNumber);
+        } else {
+            System.out.println("You pulled O" + randomNumber);
+        }
+        return randomNumber;
     }
 }
--- src/BingoCard.java
+++ src/BingoCard.java
@@ -6,16 +6,19 @@ import java.util.Scanner;
 public class BingoCard {
     private String name = "";
     private int[][] numbers;
+    public static List<BingoCard> bingoCards;
 
+    // Constructor to initialize BingoCard with name and numbers
     public BingoCard(String n, int[][] num) {
         this.name = n;
         this.numbers = num;
     }
 
+    // Static method to read Bingo cards from a file
     public static List<BingoCard> readFromFile(File file) throws Exception {
         Scanner scanner = new Scanner(file);
         List<BingoCard> cards = new ArrayList<>();
-        
+
         while (scanner.hasNextLine()) {
             String name = scanner.nextLine();
             int[][] numbers = new int[5][5];
@@ -34,23 +37,87 @@ public class BingoCard {
         }
 
         scanner.close();
+        bingoCards = cards;
         return cards;
     }
 
+    // Getter for name
     public String getName() {
         return name;
     }
 
+    // Getter for numbers array
     public int[][] getNumbers() {
         return numbers;
     }
 
-    public void printCard() {
-        System.out.println("BingoCard Name: " + name);
+    // Method to cross off a number on the Bingo card
+    public void crossOff(int num) {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                System.out.print(numbers[i][j] + "\t");
+                if (num == numbers[i][j]) {
+                    numbers[i][j] = -1;
+                }
             }
+        }
+    }
+
+    // Method to evaluate if the Bingo card has a winning combination
+    public boolean evaluate() {
+        boolean overall = false;
+        // Check rows
+        for (int i = 0; i < 5; i++) {
+            boolean rowBingo = true;
+            for (int j = 0; j < 5; j++) {
+                if (numbers[i][j] != -1) {
+                    rowBingo = false;
+                }
+            }
+            if (rowBingo) return true;
+        }
+
+        // Check columns
+        for (int j = 0; j < 5; j++) {
+            boolean colBingo = true;
+            for (int i = 0; i < 5; i++) {
+                if (numbers[i][j] != -1) {
+                    colBingo = false;
+                }
+            }
+            if (colBingo) return true;
+        }
+
+        // Check diagonals
+        boolean diagonalBingo1 = true;
+        boolean diagonalBingo2 = true;
+        for (int i = 0; i < 5; i++) {
+            if (numbers[i][i] != -1) {
+                diagonalBingo1 = false;
+            }
+            if (numbers[i][4 - i] != -1) {
+                diagonalBingo2 = false;
+            }
+        }
+        return diagonalBingo1 || diagonalBingo2;
+    }
+
+    // Method to print the Bingo card
+    public void printCard() {
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("BingoCard Name: " + name);
+        System.out.println("|\tB\t|\tI\t|\tN\t|\tG\t|\tO\t|");
+        System.out.println("---------------------------------------------------------------------------------");
+        for (int i = 0; i < 5; i++) {
+            System.out.print("|\t");
+            for (int j = 0; j < 5; j++) {
+                if (numbers[i][j] == -1) {
+                    System.out.print("XX\t|\t");
+                } else {
+                    System.out.print(numbers[i][j] + "\t|\t");
+                }
+            }
+            System.out.println();
+            System.out.print("---------------------------------------------------------------------------------");
             System.out.println();
         }
     }
