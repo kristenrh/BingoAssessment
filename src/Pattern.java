80c9cdcf7b8b7c73e9d92d141f8beb9bf710bd5f
Kristen Henningfeld <henningfeldk3407@my.uwstout.edu>
2025-03-24 16:07:05 -0500

    Added pattern class and constructor

---
+++ src/Pattern.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class Pattern {
+    private boolean[][] pattern;
+    private List<int[]> customPattern;
+
+    /**
+     * Constructor to initialize the pattern.
+     * @param pattern The 2D array containing the pattern.
+     */
+    public Pattern(boolean[][] pattern) {
+        this.pattern = pattern;
+    }
+
+}
