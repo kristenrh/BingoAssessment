d7cf34635af60d5aea55cf59e4fad255f68303e5
Kristen Henningfeld <henningfeldk3407@my.uwstout.edu>
2025-03-24 17:38:59 -0500

    added P8

--- src/PatternTest.java
+++ src/PatternTest.java
@@ -114,5 +114,22 @@ public class PatternTest {
         };
         card = new BingoCard("Test Card", numbers);
         assertTrue(pattern.checkColumn(card));
+
+        //P8: 1st row fully marked
+        for (int i = 0; i < 5; i++) {
+            columnPattern[i][4] = false;
+            columnPattern[0][i] = true;
+        }
+        pattern = new Pattern(columnPattern);
+        numbers = new int[][]{
+            {-1, -1, -1, -1, -1},
+            {6, 7, 8, 9, 10},
+            {11, 12, 13, 14, 15},
+            {16, 17, 18, 19, 20},
+            {21, 22, 23, 24, 25}
+        };
+        card = new BingoCard("Test Card", numbers);
+        assertFalse(pattern.checkColumn(card));
+
     }
 }
