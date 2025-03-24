ca4f0048c2ab83fc39252a897692e5bfade80461
Kristen Henningfeld <henningfeldk3407@my.uwstout.edu>
2025-03-24 18:50:28 -0500

    Added BingoManager class and constructor

---
+++ src/BingoManager.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BingoManager {
+    private List<Pattern> patterns;
+
+    public BingoManager() {
+        this.patterns = new ArrayList<>();
+    }
+}
