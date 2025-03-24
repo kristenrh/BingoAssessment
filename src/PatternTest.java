96f73837a5d4c3bb842d9604155afa3e5072d3f6
Kristen Henningfeld <henningfeldk3407@my.uwstout.edu>
2025-03-24 17:07:37 -0500

    added P3

--- src/PatternTest.java
+++ src/PatternTest.java
@@ -37,5 +37,21 @@ public class PatternTest {
         };
         card = new BingoCard("Test Card", numbers);
         assertTrue(pattern.checkRow(card));
+
+        // P3: Last row fully marked
+        rowPattern = new boolean[5][5];
+        for (int i = 0; i < 5; i++) {
+            rowPattern[4][i] = true;
+        }
+        pattern = new Pattern(rowPattern);
+        numbers = new int[][]{
+            {1, 2, 3, 4, 5},
+            {6, 7, 8, 9, 10},
+            {11, 12, 13, 14, 15},
+            {16, 17, 18, 19, 20},
+            {-1, -1, -1, -1, -1}
+        };
+        card = new BingoCard("Test Card", numbers);
+        assertTrue(pattern.checkRow(card));
     }
 }
