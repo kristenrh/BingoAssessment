51e94df897da9d373633c4b3e81e47c4654f9c7d
Kristen Henningfeld <henningfeldk3407@my.uwstout.edu>
2025-03-24 18:55:59 -0500

    countBingos method created

--- src/BingoManager.java
+++ src/BingoManager.java
@@ -7,7 +7,7 @@ public class BingoManager {
     public BingoManager() {
         this.patterns = new ArrayList<>();
     }
-    
+
     /**
      * Method to add a pattern for valid bingos.
      * @param pattern The pattern to add.
@@ -16,4 +16,18 @@ public class BingoManager {
         patterns.add(pattern);
     }
 
+    /**
+     * Method to count the number of bingos found on a card.
+     * @param card The BingoCard to check.
+     * @return The number of bingos found.
+     */
+    public int countBingos(BingoCard card) {
+        int bingoCount = 0;
+        for (Pattern pattern : patterns) {
+            if (pattern.matches(card)) {
+                bingoCount++;
+            }
+        }
+        return bingoCount;
+    }
 }
