dbb790a62cc0ed0ee870196a2f0e6fcc9f1276cb
Kristen Henningfeld <henningfeldk3407@my.uwstout.edu>
2025-03-24 16:16:16 -0500

    added matches method for custom patterns

--- src/Pattern.java
+++ src/Pattern.java
@@ -27,4 +27,34 @@ public class Pattern {
     public boolean[][] getPattern() {
         return pattern;
     }
+
+    /**
+     * Method to check if a card matches the pattern.
+     * @param card The BingoCard to check.
+     * @return True if the card matches the pattern, false otherwise.
+     */
+    public boolean matches(BingoCard card) {
+        int[][] numbers = card.getNumbers();
+        if (pattern != null) { //check for pattern
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 5; j++) {
+                    if (pattern[i][j] && numbers[i][j] != -1) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        } else if (customPattern != null) { //check for custom pattern
+            for (int[] coordinate : customPattern) {
+                int x = coordinate[0];
+                int y = coordinate[1];
+                if (numbers[x][y] != -1) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
 }
