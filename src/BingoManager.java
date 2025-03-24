cc59315b73ef06a63446ec28a4334f8366a64bf4
Kristen Henningfeld <henningfeldk3407@my.uwstout.edu>
2025-03-24 18:52:47 -0500

    addPattern method created

--- src/BingoManager.java
+++ src/BingoManager.java
@@ -7,4 +7,13 @@ public class BingoManager {
     public BingoManager() {
         this.patterns = new ArrayList<>();
     }
+    
+    /**
+     * Method to add a pattern for valid bingos.
+     * @param pattern The pattern to add.
+     */
+    public void addPattern(Pattern pattern) {
+        patterns.add(pattern);
+    }
+
 }
