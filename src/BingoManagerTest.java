bf2aa2f33113ede42fe9c3a3b5b8eb349a5d8a04
Kristen Henningfeld <henningfeldk3407@my.uwstout.edu>
2025-03-24 19:04:57 -0500

    added BM1

--- src/BingoManagerTest.java
+++ src/BingoManagerTest.java
@@ -4,4 +4,27 @@ import java.util.Arrays;
 import java.util.List;
 
 public class BingoManagerTest {
+    @Test
+    public void testRowPatterns() {
+        BingoManager bingoManager = new BingoManager();
+        //Add row patterns
+        for (int i = 0; i < 5; i++) {
+            boolean[][] rowPattern = new boolean[5][5];
+            for (int j = 0; j < 5; j++) {
+                rowPattern[i][j] = true;
+            }
+            bingoManager.addPattern(new Pattern(rowPattern));
+        }
+
+        //BM1: 1st row fully marked
+        int[][] numbers1 = {
+            {-1, -1, -1, -1, -1},
+            {1, 2, 3, 4, 5},
+            {6, 7, 8, 9, 10},
+            {11, 12, 13, 14, 15},
+            {16, 17, 18, 19, 20}
+        };
+        BingoCard card1 = new BingoCard("Test Card 1", numbers1);
+        assertEquals(1, bingoManager.countBingos(card1));
+    }
 }
